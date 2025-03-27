1ec678dac904e38bf6f18ef23b09334d2bfac3dc
harish-nagubadi <Harish.N@veots.com>
2025-03-27 12:48:21 +0530

    deployment

--- user/src/axios.js
+++ user/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:5000/api" 
+const BASE_URL = "https://hospital-management-xv96.onrender.com/api" 
 
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
